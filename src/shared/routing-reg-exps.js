2618a5ef32239273ac517b201947307bc1014a64
Oliver Joseph Ash <oliverjash@gmail.com>
2015-12-24 16:37:04 +0000

    Tighten reg exp for route

--- src/shared/routing-reg-exps.js
+++ src/shared/routing-reg-exps.js
@@ -1,3 +1,3 @@
 export const homeRegExp = /^\/$/;
-export const postPrefixRegExp = /^\/(\d{4})\/(\d{2})\/(\d{2})\/(.[^/]*)/;
+export const postPrefixRegExp = /^\/(\d{4})\/(\d{2})\/(\d{2})\/([a-z0-9-]*)/;
 export const postRegExp = new RegExp(postPrefixRegExp.source + /$/.source);
