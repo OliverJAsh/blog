91784cf80239439f0b68deaedd5945c4a019ecc0
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-09 17:02:45 +0000

    Improve design

---
+++ src/css.js
@@ -0,0 +1,31 @@
+export default `
+html {
+    font-family: "Lora", "Georgia", serif;
+    font-size: 18px;
+    line-height: 1.4;
+    color: hsl(0, 0%, 15%);
+    -webkit-font-smoothing: antialiased;
+}
+body {
+    /* Reset composed: UA */
+    margin: 1rem;
+    max-width: 34rem;
+}
+h1, h2, h3, h4, h5, h6 {
+    /* Reset inherited: html */
+    color: black;
+}
+h3, h4, h5, h6 {
+    /* Reset inherited: html */
+    font-family: "Helvetica Neue", "Helvetica", sans-serif;
+    font-weight: 500;
+}
+a {
+    color: hsl(0, 50%, 50%);
+}
+a:visited {
+    color: hsl(0, 50%, 35%);
+}
+pre { overflow-x: auto; }
+img { max-width: 100%; }
+`;
--- src/views/main.js
+++ src/views/main.js
@@ -1,15 +1,29 @@
 import h from 'virtual-dom/h';
+import css from '../css.js';
+
+const siteTitle = 'Oliver Joseph Ash';
+const fontsStylesheetUrl = 'https://fonts.googleapis.com/css?family=Lora:400,700';
 
 export default ({ title, body }) => (
     h('html', [
         h('head', [
-            h('title', `${title ? (title + ' – ') : ''}Blog`),
+            h('title', `${title ? (title + ' – ') : ''}${siteTitle}`),
             h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }),
-            h('style', 'body { max-width: 34rem; } pre { overflow-x: auto; } img { max-width: 100%; }')
+            h('style', { innerHTML: css }),
+            h('link', {
+                rel: 'stylesheet',
+                href: fontsStylesheetUrl,
+                media: 'none',
+                attributes: { onload: 'if(media!=\'all\')media=\'all\'' }
+            }),
+            h('noscript', h('link', {
+                rel: 'stylesheet',
+                href: fontsStylesheetUrl
+            }))
         ]),
         h('body', [
             h('h1', [
-                h('a', { href: '/' }, 'Blog')
+                h('a', { href: '/' }, siteTitle)
             ]),
             h('div', [ body ])
         ])
