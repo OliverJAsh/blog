23248c2babb083c4902cf766123e08874e5ea548
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-17 15:38:58 +0000

    Tidy up

--- src/views/main.js
+++ src/views/main.js
@@ -1,8 +1,20 @@
 import h from 'virtual-dom/h';
 import css from '../css.js';
 
+const renderNonBlockingCss = href => (
+    h('link', {
+        rel: 'stylesheet',
+        href,
+        media: 'none',
+        attributes: { onload: 'if(media!=\'all\')media=\'all\'' }
+    }),
+    h('noscript', h('link', {
+        rel: 'stylesheet',
+        href
+    }))
+);
+
 const siteTitle = 'Oliver Joseph Ash';
-const fontsStylesheetUrl = 'https://fonts.googleapis.com/css?family=Lora:400,700';
 
 export default ({ title, body }) => (
     h('html', [
@@ -11,16 +23,7 @@ export default ({ title, body }) => (
             h('title', `${title ? (title + ' – ') : ''}${siteTitle}`),
             h('meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }),
             h('style', { innerHTML: css }),
-            h('link', {
-                rel: 'stylesheet',
-                href: fontsStylesheetUrl,
-                media: 'none',
-                attributes: { onload: 'if(media!=\'all\')media=\'all\'' }
-            }),
-            h('noscript', h('link', {
-                rel: 'stylesheet',
-                href: fontsStylesheetUrl
-            }))
+            renderNonBlockingCss('https://fonts.googleapis.com/css?family=Lora:400,700')
         ]),
         h('body', [
             h('h1', [
