7670e485b426c8b0926f098f56a9c9c5a7b179a3
Oliver Joseph Ash <oliverjash@gmail.com>
2016-03-26 21:11:31 +0000

    Update code and video

--- src/posts/2016-03-26-morph-animation-between-positions-using-flip.js
+++ src/posts/2016-03-26-morph-animation-between-positions-using-flip.js
@@ -4,8 +4,8 @@ export default {
 <p>How could this animation be achieved using the web platform?</p>
 <p>When collapsed, the navigation button should be part of the document flow, and upon user interaction, it should expand to a fixed element which covers the screen. Significantly, I wanted the animation to be buttery smooth and play at 60fps, so I could only animate <a href="http://www.sitepoint.com/introduction-to-hardware-acceleration-css-animations/">CSS properties which supported hardware acceleration</a>.</p>
 <p>Here is the final result:</p>
-<iframe width="320" height="568" src="https://www.youtube.com/embed/CkLogp-hxTs" frameborder="0" allowfullscreen></iframe>
-<p>(Alternatively view at <a href="https://www.youtube.com/watch?v=CkLogp-hxTs">https://www.youtube.com/watch?v=CkLogp-hxTs</a>.)</p>
+<iframe width="320" height="568" src="https://www.youtube.com/embed/cUYbPFTnFLY" frameborder="0" allowfullscreen></iframe>
+<p>(Alternatively view at <a href="https://www.youtube.com/watch?v=cUYbPFTnFLY">https://www.youtube.com/watch?v=cUYbPFTnFLY</a>.)</p>
 <p>The answer was to use <a href="https://aerotwist.com/blog/flip-your-animations/">Paul Lewis’ First Last Invert Play (FLIP)</a> technique. When the user clicks the button:</p>
 <ul>
 <li>First: calculate the current position of the button in its collapsed state using <code>Element.getBoundingClientRect</code>.</li>
@@ -14,7 +14,7 @@ export default {
 <li>Play: enable CSS transitions and undo the inverted styles. The browser will then play out the animation smoothly at 60fps.</li>
 </ul>
 <p>We can use the same technique to collapse the button when it’s in its expanded state.</p>
-<p>Here is the code: <a href="http://jsbin.com/jakipid/1/edit?html,output">http://jsbin.com/jakipid/1/edit?html,output</a></p>
+<p><a href="http://jsbin.com/firucu/1/quiet">You can try it out for yourself</a>. Here is the code: <a href="http://jsbin.com/firucu/1/edit?html,output">http://jsbin.com/firucu/1/edit?html,output</a></p>
 <p>I think FLIP is a great technique for animations such as this. If you’re interested in learning more, <a href="https://aerotwist.com/blog/flip-your-animations/">go ahead and read Paul Lewis’ introduction to the technique</a>.</p>`,
     date: new Date(2016, 2, 26)
 };
