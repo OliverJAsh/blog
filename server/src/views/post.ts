4887522511dc111ec1fd01173d0de74f6f1c3ab5
Oliver Joseph Ash <oliverjash@gmail.com>
2016-05-17 13:03:11 +0100

    Add time el

--- server/src/views/post.ts
+++ server/src/views/post.ts
@@ -8,7 +8,7 @@ export default (post: Post) => {
     const body = h('article', [
         h('header', [
             h('h2', [ h('a', { href: post.href }, post.title) ]),
-            h('p', dateFormat(post.date, 'd mmmm yyyy'))
+            h('p', [ h('time', dateFormat(post.date, 'd mmmm yyyy')) ])
         ]),
         h('div', { innerHTML: post.body }, [])
     ]);
